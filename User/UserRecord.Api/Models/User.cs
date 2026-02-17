599d8a0e46f19708e743b94e7133c085ded03958
Ambika2112 <ambikarajputh@outlook.com>
2026-02-17 23:37:40 +0000

    Model

---
+++ User/UserRecord.Api/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace UserRecord.Api.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+    }
+}
